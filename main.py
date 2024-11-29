9787df45938a77776f45b1ea767e4f470fbd402c
yatomikazuchi <caioevilabner@gmail.com>
2024-11-28 22:47:53 -0300

    tkinter base

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-# pyPDF2, NLTK spaCY
+import tkinter as tk
+
+janela = tk.Tk()
+janela.geometry("300x300")
+janela.title("PDFast")
+
+grid_frame = tk.Frame(janela)
+grid_frame.pack(expand=True, fill="both")
+
+grid_frame.grid_rowconfigure([0, 1, 2], weight=1)
+grid_frame.grid_columnconfigure([0, 1, 2], weight=1)
+
+titulo = tk.Label(grid_frame, text="PDFast", font=("Arial", 24))
+titulo.grid(row=0, column=1)
+
+explicacao = tk.Label(grid_frame, text=" ")
+titulo.grid(row=1, column=1)
+
+botao = tk.Button(grid_frame, text="Escolher PDF")
+botao.grid(row=2, column=1)
+
+janela.mainloop()
