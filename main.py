4807a494e9fd3697456af44bb4cc06f0d38a9759
yatomikazuchi <caioevilabner@gmail.com>
2024-11-29 09:05:18 -0300

    função para criar pop up e texto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import Toplevel
+from tkinter import Label
 import os
 
 janela = tk.Tk()
@@ -18,12 +20,19 @@ def EscolherPDF():
         arquivo_nome = os.path.basename(caminho_arquivo)
     selecionado_texto.set(arquivo_nome)
 
+def Resumo():
+    janela_resumo = Toplevel()
+    janela_resumo.geometry("500x500")
+    janela_resumo.title("Análise PDF")
+    resumo_label = Label(janela_resumo,text="aaa")
+    resumo_label.pack()
+
 
 grid_frame = tk.Frame(janela)
 grid_frame.pack(expand=True, fill="both")
 
-grid_frame.grid_rowconfigure([0, 1, 2, 3], weight=1)
-grid_frame.grid_columnconfigure([0, 1, 2, 3], weight=1)
+grid_frame.grid_rowconfigure([0, 1, 2, 3, 4], weight=1)
+grid_frame.grid_columnconfigure([0, 1, 2, 3, 4], weight=1)
 
 titulo = tk.Label(grid_frame, text="PDFast", font=("Arial", 24))
 titulo.grid(row=0, column=1)
@@ -38,5 +47,8 @@ selecionado_texto = tk.StringVar()
 selecionado_texto.set(" ")
 selecionado = tk.Label(grid_frame, textvariable=selecionado_texto)
 selecionado.grid(row=3, column=1)
+
+botaoenviar = tk.Button(grid_frame, text="Analizar pdf", command=Resumo)
+botaoenviar.grid(row=4, column=1)
 
 janela.mainloop()
